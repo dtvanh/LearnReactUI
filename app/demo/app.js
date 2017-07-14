b2f8b6c40c2f7c8b53f614700b3f787e0656ef80
AnhDuong <anhduong@Dungs-iMac.local>
2017-07-14 17:16:28 +0700

    Add files

---
+++ app/demo/app.js
@@ -0,0 +1,154 @@
+import React, {
+	Component,
+} from 'react';
+import {
+	AppRegistry,
+	ListView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	TouchableHighlight,
+	View
+} from 'react-native';
+
+import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view';
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			basic: true,
+			listViewData: Array(20).fill('').map((_,i)=>`item #${i}`)
+		};
+	}
+
+	deleteRow(secId, rowId, rowMap) {
+		rowMap[`${secId}${rowId}`].closeRow();
+		const newData = [...this.state.listViewData];
+		newData.splice(rowId, 1);
+		this.setState({listViewData: newData});
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+
+
+
+				{
+					this.state.basic &&
+
+					<SwipeListView
+						dataSource={this.ds.cloneWithRows(this.state.listViewData)}
+						renderRow={ data => (
+
+								<TouchableHighlight
+									onPress={ _ => console.log('You touched me') }
+									style={styles.rowFront}
+									underlayColor={'orange'}
+								>
+									<View>
+										<Text>Fix broken park bench</Text>
+										<Text>12 Jones St</Text>
+									</View>
+								</TouchableHighlight>
+
+						)}
+
+						renderHiddenRow={ (data, secId, rowId, rowMap) => (
+							<View style={styles.rowBack}>
+								<TouchableOpacity style={[styles.backRightBtn, styles.backRightBtnRight]} onPress={ _ => this.deleteRow(secId, rowId, rowMap) }>
+									<Text style={styles.backTextWhite}>Start</Text>
+								</TouchableOpacity>
+							</View>
+						)}
+						leftOpenValue= {0}
+						rightOpenValue={-100}
+					/>
+				}
+		</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: 'white',
+		flex: 1
+	},
+
+	standalone: {
+		marginTop: 30,
+		marginBottom: 30,
+	},
+	standaloneRowFront: {
+		alignItems: 'center',
+		backgroundColor: '#CCC',
+		justifyContent: 'center',
+		height: 100,
+	},
+	standaloneRowBack: {
+		alignItems: 'center',
+		backgroundColor: '#8BC645',
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		padding: 15
+	},
+	backTextWhite: {
+		color: '#FFF'
+	},
+	rowFront: {
+		alignItems: 'center',
+		backgroundColor: '#CCC',
+		marginBottom: 5,
+		justifyContent: 'center',
+		height: 100,
+	},
+	rowBack: {
+		alignItems: 'center',
+		backgroundColor: '#DDD',
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingLeft: 15,
+		marginBottom: 5
+
+	},
+	backRightBtn: {
+		alignItems: 'center',
+		bottom: 0,
+		justifyContent: 'center',
+		position: 'absolute',
+		top: 0,
+		width: 100
+	},
+	backRightBtnLeft: {
+		backgroundColor: 'blue',
+		right: 75
+	},
+	backRightBtnRight: {
+		backgroundColor: 'orange',
+		right: 0
+	},
+	controls: {
+		alignItems: 'center',
+		marginBottom: 30
+	},
+	switchContainer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		marginBottom: 5
+	},
+	switch: {
+		alignItems: 'center',
+		borderWidth: 1,
+		borderColor: 'black',
+		paddingVertical: 10,
+		width: 100,
+	}
+});
+
+export default App;
